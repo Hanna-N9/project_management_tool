59163c95d141643730689988b1bbca9b2c8c0a2f
Hanna Negash <hnegash.n@gmail.com>
2024-02-04 20:01:55 -0800

    Add for backend

---
+++ server/app.py
@@ -0,0 +1,332 @@
+# Remote library imports
+from flask import Flask, request, make_response, session, jsonify
+from flask_restful import Resource, Api
+from datetime import datetime
+
+# Local imports
+from config import app, db, api
+
+# Import Models 
+from models import User, Project, Task, Comment
+
+@app.route("/")
+def index():
+    return "<h1>Project Management Tools</h1>"
+
+
+                            ################################# User Authentication #################################
+
+
+class Signup(Resource): 
+    def post(self):
+        try:
+            data = request.get_json()
+            user = User(
+                username=data.get("username"),
+                email=data.get("email"), 
+            )
+            user.password_hash = data.get("password")
+            db.session.add(user)
+            db.session.commit()
+            session["user_id"] = user.id
+            return make_response(user.to_dict(), 201)
+        except ValueError as e:
+            return make_response({"error": f"{e}"}, 400)
+        
+
+class SignIn(Resource):
+    def post(self):
+        username = request.get_json()["username"]
+        password = request.get_json()["password"]
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.authenticate(password):
+            session["user_id"] = user.id
+            return user.to_dict(), 200
+        session.clear()
+        return {"error": "Incorrect username or password"}, 401
+
+
+class SignOut(Resource):
+    def delete(self):
+        session.clear()
+        return {}, 204
+
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.get(session.get("user_id"))
+        if user:
+            return user.to_dict(), 200
+        else:
+            return {}, 401
+        
+        
+                             ################################# User #################################
+                             
+     
+class UserResource(Resource):
+    def get(self):
+        try:
+            return make_response([user.to_dict() for user in User.query.all()], 200)
+        except Exception as e:
+            return make_response({"Error": "Could not get data"}, 400)
+        
+
+class UsersById(Resource):
+    def get(self, id):
+        user = User.query.get(id)
+        if user:
+            return make_response(user.to_dict(), 200)
+        return make_response({"error": "User not found"}, 404)
+    
+    
+                              ################################# Project #################################
+                              
+    
+class Projects(Resource):
+    def get(self):
+        projects = [project.to_dict() for project in Project.query.all()]
+        if not projects:
+            return make_response({"error": "No projects found."}, 404)
+        return make_response(jsonify(projects), 200)
+    
+    
+    def post(self):
+        try:
+            data = request.get_json()
+
+            start_date = data.get("startDate")
+            end_date = data.get("endDate")
+
+            if start_date is None or end_date is None:
+                return make_response({"error": "Missing start_date or end_date."}, 400)
+
+            # Checked if the user exists by using the user ID from the session
+            user = User.query.get(session.get("user_id"))
+            if user is None:
+                return make_response({"error": "User not found."}, 404)
+
+            new_project = Project(
+                user_id=data.get("user_id"),
+                title=data.get("title"),               
+                description=data.get("description"),
+                status=data.get("status"),
+                start_date=datetime.strptime(start_date, "%Y-%m-%d"),
+                end_date=datetime.strptime(end_date, "%Y-%m-%d"),
+            )
+            db.session.add(new_project)
+            db.session.commit()
+            return make_response(new_project.to_dict(), 201)
+        except ValueError as e:
+            return make_response({"error": e.__str__()}, 400)
+        except KeyError:
+            return make_response({"error": "Missing required data."}, 400)
+        
+        
+class ProjectId(Resource):
+    def get(self, id):
+        project = Project.query.get(id)
+        if project:
+            return make_response(project.to_dict(), 200)
+        return make_response({"error": "Project not found."}, 404)
+    
+    
+    def patch(self, id):
+        project = Project.query.get(id)
+        if not project:
+            return make_response({"error": "Project not found."}, 404)
+        else:
+            data = request.get_json()
+            try:    
+                for attr, value in data.items():
+                    if hasattr(project, attr):
+                        setattr(project, attr, value)
+
+                db.session.add(project)
+                db.session.commit()
+
+                return make_response(project.to_dict(), 202)
+            except ValueError as e:
+                return make_response({"error": e.__str__()}, 400)
+            
+            
+    def delete(self, id):
+        project = Project.query.get(id)
+        if not project:
+            return make_response({"error": "Project not found."}, 404)
+        else:
+            db.session.delete(project)
+            db.session.commit()
+            return make_response({"message": "Project deleted successfully."}, 204)
+        
+        
+                                    ################################# Task #################################
+        
+        
+class Tasks(Resource):
+    def get(self):
+        tasks = [task.to_dict() for task in Task.query.all()]
+        if not tasks:
+            return make_response({"error": "No tasks found."}, 404)
+        return make_response(jsonify(tasks), 200)
+ 
+     
+    def post(self):
+        try:
+            data = request.get_json()
+            
+            due_date = data.get("dueDate")
+
+            if due_date is None:
+                return make_response({"error": "Missing due date."}, 400)
+            
+            # Checked if the user exists by using the user ID from the session
+            user = User.query.get(session.get("user_id"))
+            if user is None:
+                return make_response({"error": "User not found."}, 404)
+
+            new_task = Task(
+                user_id=data.get("user_id"),
+                description=data.get("description"),
+                due_date=datetime.strptime( due_date, "%Y-%m-%d"),
+                priority=data.get("priority"),
+                status=data.get("status"),
+            )
+            db.session.add(new_task)
+            db.session.commit()
+            return make_response(new_task.to_dict(), 201)
+        except ValueError as e:
+            return make_response({"error": e.__str__()}, 400)
+        except KeyError:
+            return make_response({"error": "Missing required data."}, 400)
+        
+        
+
+class TaskId(Resource):
+    def get(self, id):
+        task = Task.query.get(id)
+        if task:
+            return make_response(task.to_dict(), 200)
+        return make_response({"error": "Task not found."}, 404)
+    
+    
+    def patch(self, id):
+        task = Task.query.get(id)
+        if not task:
+            return make_response({"error": "Task not found."}, 404)
+        else:
+            data = request.get_json()
+            data["due_date"] = datetime.strptime(data["due_date"], "%Y-%m-%d")
+            try:
+                for attr, value in data.items():
+                    if hasattr(task, attr):
+                        setattr(task, attr, value)
+
+                db.session.add(task)
+                db.session.commit()
+
+                return make_response(task.to_dict(), 202)
+            except ValueError as e:
+                return make_response({"error": e.__str__()}, 400)
+            
+            
+    def delete(self, id):
+        task = Task.query.get(id)
+        if not task:
+            return make_response({"error": "Task not found."}, 404)
+        else:
+            db.session.delete(task)
+            db.session.commit()
+            return make_response({"message": "Task deleted successfully."}, 204)
+        
+        
+                                ################################# Comment #################################
+
+
+class Comments(Resource):
+    def get(self):
+        comments = [comment.to_dict() for comment in Comment.query.all()]
+        if not comments:
+            return make_response({"error": "No comments found."}, 404)
+        return make_response(jsonify(comments), 200)
+    
+ 
+    def post(self):
+        try:
+            data = request.get_json()
+            user = User.query.get(session.get("user_id"))
+            
+            # Checked if the user exists by using the user ID from the session
+            if user is None:
+                return make_response({"error": "User not found."}, 404)
+            
+            new_comment = Comment(
+                user_id=data.get("user_id"),
+                task_id=data.get("task_id"),
+                text=data.get("text"),
+            )
+            db.session.add(new_comment)
+            db.session.commit()
+            return make_response(new_comment.to_dict(), 201)
+        except ValueError as e:
+            return make_response({"error": e.__str__()}, 400)
+        except KeyError:
+            return make_response({"error": "Missing required data."}, 400)
+        
+
+class CommentId(Resource):
+    def get(self, id):
+        comment = Comment.query.get(id)
+        if comment:
+            return make_response(comment.to_dict(), 200)
+        return make_response({"error": "Comment not found."}, 404)
+    
+        
+    def patch(self, id):
+        comment = Comment.query.get(id)
+        if not comment:
+            return make_response({"error": "Comment not found"}, 404)
+        data = request.get_json()
+        try:
+            for attr, value in data.items():
+                if hasattr(comment, attr):
+                    setattr(comment, attr, value)
+                
+            db.session.add(comment)
+            db.session.commit()
+            return make_response(comment.to_dict(), 202)
+        except ValueError:
+            return make_response({"errors": ["validation errors"]}, 400)
+            
+            
+    def delete(self, id):
+        comment = Comment.query.get(id)
+        if not comment:
+            return make_response({"error": "Comment not found."}, 404)
+        else:
+            db.session.delete(comment)
+            db.session.commit()
+            return make_response({"message": "Comment deleted successfully."}, 204)  
+        
+        
+api.add_resource(Signup, "/sign_up")
+api.add_resource(SignIn, "/signIn")
+api.add_resource(SignOut, "/signOut")
+api.add_resource(CheckSession, "/check_session")
+api.add_resource(UserResource, "/user")
+api.add_resource(UsersById, "/user/<int:id>")
+api.add_resource(Projects, "/projects")
+api.add_resource(ProjectId, "/projects/<int:id>")
+api.add_resource(Tasks, "/tasks")
+api.add_resource(TaskId, "/tasks/<int:id>")
+api.add_resource(Comments, "/comments")
+api.add_resource(CommentId, "/comments/<int:id>")
+
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
+    
+    
+
+   
---
+++ server/models.py
@@ -0,0 +1,222 @@
+from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.ext.hybrid import hybrid_property
+from sqlalchemy.orm import validates
+from datetime import datetime
+
+# Local imports
+
+from config import db, bcrypt
+
+
+# A many-to-many relationship between the User and Project models through the user_project_association table
+user_project_association = db.Table('user_project',
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('project_id', db.Integer, db.ForeignKey('projects.id'), primary_key=True)
+)
+
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=False)
+
+    # Relationships
+    projects = db.relationship("Project", secondary=user_project_association, back_populates="users")
+    comments = db.relationship("Comment", back_populates="user", cascade="all, delete-orphan")
+    comment_texts = association_proxy("comments", "text")
+    
+    # Serialization rules
+    serialize_rules = ("-password_hash", "-projects", "-comments")
+    
+    # Validation    
+    @validates("username")
+    def validate_username(self, key, username):
+        if not isinstance(username, str):
+            raise ValueError(f"{key} must be a string.")
+        if not username or len(username) < 3: 
+            raise ValueError("Username must be longer than 3 characters.")
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            raise ValueError("Username must be unique.")
+        return username
+    
+    @validates("email")
+    def validate_email(self, key, email):
+        if not isinstance(email, str):
+            raise ValueError(f"{key} must be a string.")
+        if not email or len(email) < 1: 
+            raise ValueError("Email must not be empty.")
+        existing_user = User.query.filter_by(email=email).first()
+        if existing_user:
+            raise ValueError("Email must be unique.")
+        return email
+
+    #prevent direct access to the hashed password
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hashes are private.")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(password.encode("utf-8"))
+        self._password_hash = password_hash.decode("utf-8")
+        
+    #uses bcrypt to compare the provided password with the stored hash   
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+    def __repr__(self):
+        return f"User {self.username}"
+        
+
+# Project a many-to-one relationship with the User model and a one-to-many relationship with the Task model. 
+class Project(db.Model, SerializerMixin):
+    __tablename__ = "projects"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False) # A brief description of the project
+    start_date = db.Column(db.DateTime, nullable=False) # The date when the project starts
+    end_date = db.Column(db.DateTime, nullable=False) # The date when the project ends
+    status = db.Column(db.String, nullable=False) # Current status of the project - like "Not Started", "In Progress", "Completed"
+
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    # Relationships
+    users = db.relationship("User", secondary=user_project_association, back_populates="projects")
+    tasks = db.relationship("Task", back_populates="project", cascade="all, delete-orphan")
+    
+    
+    # Serialization rules
+    serialize_rules = ("-user_id", "tasks")
+    
+     # Validation
+    @validates('title')
+    def validate_title(self, key, title):
+        if not isinstance(title, str):
+            raise ValueError(f"{key} must be a string.")
+        if not title or len(title) < 1:
+            raise ValueError("Project must have a title.")
+        if len(title) > 100:
+            raise ValueError("Title must be shorter than 100 characters.")
+        return title
+
+    @validates('description')
+    def validate_description(self, key, description):
+        if not isinstance(description, str):
+            raise ValueError(f"{key} must be a string.")
+        if not description or len(description) < 1:
+            raise ValueError("Description must not be empty.")
+        if len(description) > 500:
+            raise ValueError("Description must be shorter than 500 characters.")
+        return description
+
+    @validates('start_date', 'end_date')
+    def validate_date(self, key, date):
+        if not isinstance(date, datetime):
+            raise ValueError(f"{key} must be a datetime object.")
+        if date < datetime.utcnow():
+            raise ValueError(f"{key} must not be in the past.")
+        return date
+
+    @validates('status')
+    def validate_status(self, key, status):
+        valid_statuses = ["Not Started", "In Progress", "Completed"]
+        if status not in valid_statuses:
+            raise ValueError(f"{key} must be one of {valid_statuses}")
+        return status
+    
+    
+    def __repr__(self):
+        return f"Project #{self.id} : {self.title}"
+
+
+    
+class Task(db.Model, SerializerMixin):
+    __tablename__ = "tasks"
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String, nullable=False) # A brief description of the task, user stories --- smaller unit of work from project
+    due_date = db.Column(db.Date, nullable=False) # When the task is due
+    priority = db.Column(db.String, nullable=False) # priority level of the task - like "High", "Medium", "Low"
+    status = db.Column(db.String, nullable=False) # Current status of the task - like "Not Started", "In Progress", "Completed"
+    
+    project_id = db.Column(db.Integer, db.ForeignKey("projects.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+
+    # Relationships
+    project = db.relationship("Project", back_populates="tasks", cascade="all, delete-orphan")
+    comments = db.relationship("Comment", back_populates="task", cascade="all, delete-orphan")
+
+    # Serialization rules
+    serialize_rules = ("-project_id", "comments")
+
+
+    @validates('description')
+    def validate_description(self, key, description):
+        if not isinstance(description, str):
+            raise ValueError(f"{key} must be a string.")
+        if not description or len(description) < 1:
+            raise ValueError("Description must not be empty.")
+        if len(description) > 500:
+            raise ValueError("Description must be shorter than 500 characters.")
+        return description
+
+    @validates('due_date')
+    def validate_due_date(self, key, due_date):
+        if not isinstance(due_date, date):
+            raise ValueError(f"{key} must be a date object.")
+        if due_date < date.today():
+            raise ValueError(f"{key} must not be in the past.")
+        return due_date
+
+    @validates('priority')
+    def validate_priority(self, key, priority):
+        valid_priorities = ["High", "Medium", "Low"]
+        if priority not in valid_priorities:
+            raise ValueError(f"{key} must be one of {valid_priorities}")
+        return priority
+
+    @validates('status')
+    def validate_status(self, key, status):
+        valid_statuses = ["Not Started", "In Progress", "Completed"]
+        if status not in valid_statuses:
+            raise ValueError(f"{key} must be one of {valid_statuses}")
+        return status
+    
+    
+    def __repr__(self):
+        return f"Task #{self.id} | User #{self.user_id}"
+    
+    
+class Comment(db.Model, SerializerMixin):
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String, nullable=False)
+    
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    task_id = db.Column(db.Integer, db.ForeignKey("tasks.id"))
+
+    # Relationships
+    user = db.relationship("User", back_populates="comments", cascade="all, delete-orphan")
+    task = db.relationship("Task", back_populates="comments", cascade="all, delete-orphan")
+
+    # Serialization rules
+    serialize_rules = ("-user_id", "-task_id")
+    
+    @validates('text')
+    def validate_text(self, key, text):
+        if not isinstance(text, str):
+            raise ValueError(f"{key} must be a string.")
+        if not text or len(text) < 1:
+            raise ValueError("Comment must not be empty.")
+        if len(text) > 200:
+            raise ValueError("Comment must be shorter than 200 characters.")
+        return text
+    
+    def __repr__(self):
+        return f"Comment #{self.id} | User #{self.user_id}"
