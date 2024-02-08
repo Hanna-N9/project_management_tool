a37e9bf1705c2c9d02f11c90f137fec18fb7a2dc
Hanna Negash <hnegash.n@gmail.com>
2024-02-07 20:27:13 -0800

    Correctly include links to different pages

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,48 +4,30 @@ import "../App.css";
 export default function NavBar() {
   return (
     <nav>
-      <NavLink to="/" className="navLink">
-        Home Page
+      <NavLink to="/" className="navLink" exact>
+        Home
       </NavLink>
       <NavLink to="/login" className="navLink">
-        Login Page
+        Login
       </NavLink>
       <NavLink to="/sign_up" className="navLink">
-        Sign Up page
+        Sign Up
       </NavLink>
       <NavLink to="/dashboard" className="navLink">
-        Dashboard page
+        Dashboard
       </NavLink>
-      {/*  <NavLink to="/logout" className="navLink">
-        Logout page
+      <NavLink to="/projects" className="navLink">
+        Projects
       </NavLink>
-      <NavLink to="/project/:id" className="navLink">
-        Project Detail Page
+      <NavLink to="/tasks" className="navLink">
+        Tasks
       </NavLink>
-      <NavLink to="/project-form" className="navLink">
-        Project Form
+      <NavLink to="/comments" className="navLink">
+        Comments
       </NavLink>
       <NavLink to="/user" className="navLink">
         User Profile
       </NavLink>
-      <NavLink to="/admin" className="navLink">
-        Admin
-      </NavLink>
-      <NavLink to="/comment-form" className="navLink">
-        Comment Form
-      </NavLink>
-      <NavLink to="/edit-task" className="navLink">
-        Edit Task
-      </NavLink>
-      <NavLink to="/task-form" className="navLink">
-        Task Form
-      </NavLink>
-      <NavLink to="/edit-comment" className="navLink">
-        Edit Comment
-      </NavLink>
-      <NavLink to="/edit-project" className="navLink">
-        Edit Project
-      </NavLink> */}
     </nav>
   );
 }
