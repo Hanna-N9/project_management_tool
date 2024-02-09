109179575ced7a21046604eecab29e095e8f0c06
Hanna Negash <hnegash.n@gmail.com>
2024-02-08 21:58:12 -0800

    Change routes for project content

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,17 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./AuthContext";
-
 import HomePage from "./Homepage";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 import SignUp from "./SignUp";
 import Logout from "./Logout";
-import ProjectDetailPage from "./ProjectDetail";
+import Projects from "./Projects";
 import NavBar from "./NavBar";
 import AdminPage from "./AdminPage";
 import CommentForm from "./CommentForm";
 import EditComment from "./EditComment";
 import EditTask from "./EditTask";
-import ProjectForm from "./ProjectForm";
-import UserProfile from "./UserProfile";
+import User from "./User";
 import EditProject from "./EditProject";
 import TaskForm from "./TaskForm";
 
@@ -22,23 +20,22 @@ import "../App.css";
 export default function App() {
   return (
     <div className="App">
-      <NavBar />
       <AuthProvider>
+        <NavBar />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
           <Route path="/sign_up" element={<SignUp />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/project/:id" element={<ProjectDetailPage />} />
-          <Route path="/user" element={<UserProfile />} />
+          <Route path="/user" element={<User />} />
+          <Route path="/projects" element={<Projects />} />
           <Route path="/admin" element={<AdminPage />} />
           <Route path="/comment-form" element={<CommentForm />} />
           <Route path="/edit-task" element={<EditTask />} />
           <Route path="/task-form" element={<TaskForm />} />
           <Route path="/edit-comment" element={<EditComment />} />
           <Route path="/edit-project" element={<EditProject />} />
-          <Route path="/project-form" element={<ProjectForm />} />
           <Route
             path="/redirect-page"
             element={<Navigate to="/error-page" replace />}
