fe7a64c95e9b37a36b0f745b1c575d12a136f26b
Hanna Negash <hnegash.n@gmail.com>
2024-02-08 22:02:02 -0800

    Adjust routes for authentication focus

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,33 +1,44 @@
+// Navbar.js
 import { NavLink } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "./AuthContext";
 import "../App.css";
 
 export default function NavBar() {
+  const { isAuthenticated } = useContext(AuthContext);
+
   return (
     <nav>
       <NavLink to="/" className="navLink" exact>
         Home
       </NavLink>
-      <NavLink to="/login" className="navLink">
-        Login
-      </NavLink>
-      <NavLink to="/sign_up" className="navLink">
-        Sign Up
-      </NavLink>
-      <NavLink to="/dashboard" className="navLink">
-        Dashboard
-      </NavLink>
-      <NavLink to="/projects" className="navLink">
-        Projects
-      </NavLink>
-      <NavLink to="/tasks" className="navLink">
-        Tasks
-      </NavLink>
-      <NavLink to="/comments" className="navLink">
-        Comments
-      </NavLink>
-      <NavLink to="/user" className="navLink">
-        User Profile
-      </NavLink>
+      {isAuthenticated ? (
+        // Links for authenticated users
+        <>
+          <NavLink to="/projects" className="navLink">
+            Projects
+          </NavLink>
+          <NavLink to="/tasks" className="navLink">
+            Tasks
+          </NavLink>
+          <NavLink to="/comments" className="navLink">
+            Comments
+          </NavLink>
+          <NavLink to="/logout" className="navLink">
+            Logout
+          </NavLink>
+        </>
+      ) : (
+        // Links for unauthenticated users
+        <>
+          <NavLink to="/login" className="navLink">
+            Login
+          </NavLink>
+          <NavLink to="/sign_up" className="navLink">
+            Sign Up
+          </NavLink>
+        </>
+      )}
     </nav>
   );
 }
