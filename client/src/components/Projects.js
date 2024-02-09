aa04d7768d4c439cef87463aafd67697c798cc8f
Hanna Negash <hnegash.n@gmail.com>
2024-02-08 22:27:30 -0800

    See the current details while the edit form is displayed

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -52,12 +52,17 @@ export default function Projects() {
       {projects.map(project => (
         <div key={project.id}>
           {editingProjectId === project.id ? (
-            <EditProject
-              projectId={project.id}
-              initialValues={project}
-              onUpdate={handleProjectUpdate}
-              onCancel={handleStopEditing}
-            />
+            <>
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+              <p>{project.status}</p>
+              <EditProject
+                projectId={project.id}
+                initialValues={project}
+                onUpdate={handleProjectUpdate}
+                onCancel={handleStopEditing}
+              />
+            </>
           ) : (
             <>
               <h3>{project.title}</h3>
