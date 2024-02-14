499cf5471d23d06003c794040110dedd62dc3ec0
Hanna Negash <hnegash.n@gmail.com>
2024-02-14 00:26:11 -0800

    Add few details

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -1,3 +1,18 @@
+import { useAuth } from "./AuthContext";
+
 export default function HomePage() {
-  return <h1>Home Page</h1>;
+  const { isAuthenticated } = useAuth();
+
+  return (
+    <div className="main-conten">
+      <h1>Welcome to ProjectHub</h1>
+      {isAuthenticated ? (
+        <p>
+          Please click on the navigation bar to create and save your projects!
+        </p>
+      ) : (
+        <p>Please log in to create and save your projects.</p>
+      )}
+    </div>
+  );
 }
