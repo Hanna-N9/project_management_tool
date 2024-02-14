a5f5c05d73af4e5a8e3dcab597708965b3e14c31
Hanna Negash <hnegash.n@gmail.com>
2024-02-13 21:27:15 -0800

    Add useEffect hook without returning anything

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,24 +1,23 @@
-import React from "react";
+import { useEffect } from "react";
 import axios from "axios";
 import { useAuth } from "./AuthContext"; // Import the useAuth hook
 import { useNavigate } from "react-router-dom";
 
 export default function Logout() {
   const { setIsAuthenticated, setCurrentUser } = useAuth(); // Use the custom hook to get the setIsAuthenticated and setCurrentUser functions
+
   const navigate = useNavigate();
 
-  const handleLogout = () => {
+  useEffect(() => {
     axios
       .delete("/signOut")
       .then(() => {
         setIsAuthenticated(false); // Set isAuthenticated to false
         setCurrentUser(null); // Clear user data from the context
-        navigate("/"); // Redirect to the home page
+        navigate("/");
       })
       .catch(error => {
-        console.error("Network error", error); // Log any errors that occur
+        console.error("Network error", error);
       });
-  };
-
-  return <button onClick={handleLogout}>Logout</button>;
+  }, []);
 }
