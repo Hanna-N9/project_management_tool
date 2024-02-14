f755bcfcadbdab399dba4d5ff1625ad3b329f158
Hanna Negash <hnegash.n@gmail.com>
2024-02-13 21:26:01 -0800

    Remove two components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,11 +2,9 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import { AuthProvider } from "./AuthContext";
 import NavBar from "./NavBar";
 import HomePage from "./Homepage";
-import Dashboard from "./Dashboard";
-import Login from "./Login";
 import SignUp from "./SignUp";
+import Login from "./Login";
 import Logout from "./Logout";
-import AdminPage from "./AdminPage";
 import User from "./User";
 import Projects from "./Projects";
 import EditProject from "./EditProject";
@@ -27,8 +25,6 @@ export default function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/logout" element={<Logout />} />
           <Route path="/sign_up" element={<SignUp />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/admin" element={<AdminPage />} />
           <Route path="/user" element={<User />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/edit-project" element={<EditProject />} />
