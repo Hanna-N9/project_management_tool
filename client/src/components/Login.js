c09c673f2c0e9b041c0907030501eac47733af82
Hanna Negash <hnegash.n@gmail.com>
2024-02-14 22:06:23 -0800

    Change to formik and add yup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,65 +1,69 @@
-import { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import axios from "axios";
 import { useAuth } from "./AuthContext";
 import { useNavigate } from "react-router-dom";
 
-export default function Login() {
-  const [credentials, setCredentials] = useState({
-    username: "",
-    password: "",
-  });
-  const [errorMessage, setErrorMessage] = useState("");
+const LoginSchema = Yup.object().shape({
+  username: Yup.string().required("Required"),
+  password: Yup.string().required("Required"),
+});
 
+export default function Login() {
   const { setIsAuthenticated, setCurrentUser } = useAuth();
   const navigate = useNavigate();
 
-  const handleLoginChange = e => {
-    setCredentials({ ...credentials, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    axios
-      .post("/login", credentials)
-      .then(response => {
-        if (response.data.error) {
-          setErrorMessage("Invalid Username or Password, Please try again.");
-        } else {
-          console.log("Logged in successfully");
-          setIsAuthenticated(true);
-          setCurrentUser(response.data);
-          navigate("/user");
-        }
-      })
-      .catch(error => {
-        // If the error is due to incorrect credentials, display the custom message
-        if (error.response && error.response.data.error) {
-          setErrorMessage("Invalid Username or Password, Please try again.");
-        } else {
-          // For other errors, display a different message
-          setErrorMessage("An unexpected error occurred during login.");
-        }
-      });
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Username"
-        value={credentials.username}
-        onChange={handleLoginChange}
-        name="username"
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={credentials.password}
-        onChange={handleLoginChange}
-        name="password"
-      />
-      <button type="submit">Login</button>
-      {errorMessage && <div className="error-message">{errorMessage}</div>}
-    </form>
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      validationSchema={LoginSchema}
+      onSubmit={(values, actions) => {
+        axios
+          .post("/login", values)
+          .then(response => {
+            if (response.data.error) {
+              actions.setFieldError("general", response.data.error);
+            } else {
+              setIsAuthenticated(true);
+              setCurrentUser(response.data);
+              navigate("/user");
+            }
+            actions.setSubmitting(false);
+          })
+          .catch(error => {
+            actions.setSubmitting(false);
+            if (error.response) {
+              actions.setFieldError("general", error.response.data.error);
+            } else {
+              actions.setFieldError(
+                "general",
+                "An unexpected error occurred during login.",
+              );
+            }
+          });
+      }}>
+      {({ errors, touched }) => (
+        <Form>
+          <ErrorMessage
+            name="username"
+            component="div"
+            className="error-message"
+          />
+          <Field type="text" name="username" placeholder="Username" />
+
+          <ErrorMessage
+            name="password"
+            component="div"
+            className="error-message"
+          />
+          <Field type="password" name="password" placeholder="Password" />
+
+          <button type="submit">Login</button>
+          {errors.general && touched.general && (
+            <div className="error-message">{errors.general}</div>
+          )}
+        </Form>
+      )}
+    </Formik>
   );
 }
--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,76 +1,84 @@
-import { useState } from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import axios from "axios";
 import { useAuth } from "./AuthContext";
 import { useNavigate } from "react-router-dom";
 
+const SignupSchema = Yup.object().shape({
+  username: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email").required("Required"),
+  password: Yup.string()
+    .min(8, "Must be at least  8 characters")
+    .required("Required"),
+});
+
 export default function Signup() {
-  const [signupInfo, setSignupInfo] = useState({
-    username: "",
-    password: "",
-    email: "",
-  });
-  const [errorMessage, setErrorMessage] = useState("");
-  const [successMessage, setSuccessMessage] = useState("");
   const { setIsAuthenticated, setCurrentUser } = useAuth();
   const navigate = useNavigate();
 
-  const handleInputChange = e => {
-    setSignupInfo({ ...signupInfo, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = e => {
-    e.preventDefault();
-    axios
-      .post("/sign_up", signupInfo)
-      .then(response => {
-        if (response.data.error) {
-          setErrorMessage(response.data.error);
-        } else {
-          setSuccessMessage("Successfully signed up");
-          setIsAuthenticated(true); // Only set isAuthenticated to true once after a successful signup
-          setCurrentUser(response.data); // Update user in the AuthContext
-          navigate("/user"); // Redirect to user profile
-        }
-      })
-      .catch(error => {
-        if (error.response) {
-          setErrorMessage(error.response.data.error);
-        } else if (error.request) {
-          setErrorMessage("No response received from the server.");
-        } else {
-          setErrorMessage("An error occurred during signup.");
-        }
-      });
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="Username"
-        value={signupInfo.username}
-        onChange={handleInputChange}
-        name="username"
-      />
-      <input
-        type="email"
-        placeholder="Email"
-        value={signupInfo.email}
-        onChange={handleInputChange}
-        name="email"
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={signupInfo.password}
-        onChange={handleInputChange}
-        name="password"
-      />
-      <button type="submit">Sign Up</button>
-      {errorMessage && <div className="error-message">{errorMessage}</div>}
-      {successMessage && (
-        <div className="success-message">{successMessage}</div>
+    <Formik
+      initialValues={{ username: "", email: "", password: "" }}
+      validationSchema={SignupSchema}
+      onSubmit={(values, actions) => {
+        axios
+          .post("/sign_up", values)
+          .then(response => {
+            if (response.data.error) {
+              actions.setFieldError("general", response.data.error);
+            } else {
+              setIsAuthenticated(true);
+              setCurrentUser(response.data);
+              navigate("/user");
+            }
+            actions.setSubmitting(false);
+          })
+          .catch(error => {
+            actions.setSubmitting(false);
+            if (error.response) {
+              actions.setFieldError("general", error.response.data.error);
+            } else if (error.request) {
+              actions.setFieldError(
+                "general",
+                "No response received from the server.",
+              );
+            } else {
+              actions.setFieldError(
+                "general",
+                "An error occurred during signup.",
+              );
+            }
+          });
+      }}>
+      {({ errors, touched }) => (
+        <Form>
+          <ErrorMessage
+            name="username"
+            component="div"
+            className="error-message"
+          />
+          <Field type="text" name="username" placeholder="Username" />
+
+          <ErrorMessage
+            name="email"
+            component="div"
+            className="error-message"
+          />
+          <Field type="email" name="email" placeholder="Email" />
+
+          <ErrorMessage
+            name="password"
+            component="div"
+            className="error-message"
+          />
+          <Field type="password" name="password" placeholder="Password" />
+
+          <button type="submit">Sign Up</button>
+          {errors.general && touched.general && (
+            <div className="error-message">{errors.general}</div>
+          )}
+        </Form>
       )}
-    </form>
+    </Formik>
   );
 }
